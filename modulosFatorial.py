97d0d5622977daa1e9fcbc03a5707b5c7dfa6e66
Aline Grisolfi <alinegrisolfi10@gmail.com>
2023-01-16 17:38:59 -0300

    Exemplo  de como trabalhar com módulos.
    O quanto módulos ou bibliotecas podem facilitar.

---
+++ modulosFatorial.py
@@ -0,0 +1,5 @@
+'''Faça um programa que leia um número qualquer e mostre o seu fatorial.'''
+from math import factorial
+n = int(input('Digite um número para calcular seu fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}! '.format(n, f))
